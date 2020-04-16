20c026aba4e6cba4b5188a0de69857303067bd4d
huntercole25 <huntercole@u.boisestate.edu>
2020-04-16 14:45:58 -0600

    .

--- 02_LicensePlots.R
+++ 02_LicensePlots.R
@@ -24,16 +24,21 @@ YearlySum[,Population := Population_in_Thousands*1000]
 
 YearlySum[,`Percent Hunters` := `Licenses Sold`/Population * 100]
 
+ylab <- c(14, 15, 16)
+
 ggplotly(ggplot(data = YearlySum, aes(x = Year, y = `Licenses Sold`)) +
-           geom_line()
+           geom_line() +
+           scale_y_continuous(labels = paste(ylab, "million"), breaks = 1000000*ylab)
 )
 
 ggplotly(ggplot(data = YearlySum, aes(x = Year, y = `Percent Hunters`)) +
-           geom_line())
+           geom_line()
+         )
 
 ggplotly(ggplot(data = HuntingLicenses, aes(x  = Year, y = `Licenses Sold`, color = State)) +
-           geom_line()
+           geom_line() +
+           scale_y_continuous(labels = c("0", "500,000", "1,000,000"), breaks = c(0, 500000, 1000000))
 )
 
 ggplotly(ggplot(data = HuntingLicenses, aes(x = Year, y = `Percent Hunters`, color = State)) + 
-  geom_line())
+           geom_line())
